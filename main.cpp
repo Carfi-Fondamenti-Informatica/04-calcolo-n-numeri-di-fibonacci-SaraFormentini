d72434f90361a9b8017743c24a81aa62dd5d3f49
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-04-25 15:20:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-   return 0;
+   int n=0, i=0,primo=1,secondo=1,terzo,c;
+   cin>>n;
+   c=n-2;
+   if (n>=2){
+       cout<<primo<<endl;
+       cout<<secondo<<endl;
+       while(i<c) {
+           terzo = primo + secondo;
+           primo = secondo;
+           secondo = terzo;
+           i++;
+           cout << terzo << endl;
+   }}else {
+       cout<<"errore"<<endl;
+       }
+
+    return 0;
 }
